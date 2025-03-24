00cbc527072c681eaf527ead70347c0f106867c1
Goyo Vargas <gregory.vargas18@gmail.com>
2025-03-24 12:24:36 -0500

    Implement fetching nodes and formatting it into a tree structure

--- src/models/Tree.ts
+++ src/models/Tree.ts
@@ -2,15 +2,19 @@ export interface INode {
   id: number;
   parentId: number;
   label: string;
-  children: INode[];
   created: Date;
+}
+
+export interface NodeEntity {
+  id: number;
+  label: string;
+  children: NodeEntity[];
 }
 
 const DEFAULT_NODE_VALS = (): INode => ({
   id: -1,
   parentId: -1,
   label: '',
-  children: [],
   created: new Date(),
 });
 
--- src/services/TreeService.ts
+++ src/services/TreeService.ts
@@ -1,8 +1,37 @@
-import { INode } from '@src/models/Tree';
+import { NodeEntity } from '@src/models/Tree';
 import TreeRepo from '@src/repos/TreeRepo';
 
-function getTree(): Promise<INode[]> {
-  return TreeRepo.getNodes();
+async function getTree(): Promise<NodeEntity[]> {
+  const nodes = await TreeRepo.getNodes();
+
+  const nodeMap = new Map<NodeEntity['id'], NodeEntity>();
+  const defaultNode: NodeEntity = {
+    id: -1,
+    label: '',
+    children: [],
+  };
+
+  nodes.forEach((node) => {
+    nodeMap.set(node.id, {
+      id: node.id,
+      label: node.label,
+      children: [],
+    });
+  });
+
+  const rootNodes: NodeEntity[] = [];
+
+  nodes.forEach((node) => {
+    if (node.parentId < 0 && nodeMap.has(node.id)) {
+      rootNodes.push(nodeMap.get(node.id) ?? defaultNode);
+    } else if (nodeMap.has(node.parentId)) {
+      (nodeMap.get(node.parentId) ?? defaultNode).children.push(
+        nodeMap.get(node.id) ?? defaultNode
+      );
+    }
+  });
+
+  return rootNodes;
 }
 
 export default {
